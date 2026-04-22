1be0c99f002d7dfc19550e5e3ca4d2d39afa2df5
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:37:46 +0000

    int

--- models/intermediate/int_open_regions.sql
+++ models/intermediate/int_open_regions.sql
@@ -1,7 +1,9 @@
+-- calcul du pourcentage d'étudiants par régions sur l'année 2024
+
 select
-    round(count(distinct USER_ID) * 100.0 / sum(count(distinct USER_ID)) over (Partition by ANNEE_DEBUT_FORMATION),2) as POURCENTAGE_REGION_OPENCLASSROOM, 
+    round(count(USER_ID) * 100.0 / sum(count(USER_ID)) over (Partition by ANNEE_DEBUT_FORMATION),2) as POURCENTAGE_REGION_OPENCLASSROOM, 
     REGION,
     ANNEE_DEBUT_FORMATION
 from {{ ref('stg_openclassrooms') }}
-WHERE ANNEE_DEBUT_FORMATION = 2024
+WHERE ANNEE_DEBUT_FORMATION = 2024 --filtre sur l'année
 GROUP BY REGION,ANNEE_DEBUT_FORMATION
