ecccc57b6fbbbc9a1e0356a5e9765c4f97f29c03
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:37:51 +0000

    int

--- models/intermediate/int_quantite_parcours.sql
+++ models/intermediate/int_quantite_parcours.sql
@@ -1,6 +1,8 @@
+-- calcul nombre et pourcentage d'étudiants sur l'ensemble des années avec et sans les doublons (doublons = ayant fait plus de 2 formations)
+
 select
     'LIAISON' as LIEN,
-    count(DISTINCT USER_ID) as NOMBRE_ETUDIANT_TOTAL, 
-    count(USER_ID) - count(DISTINCT USER_ID) as NOMBRE_USER_TOTAL_DOUBLON,
-    round((count(USER_ID) - count(DISTINCT USER_ID)) * 100.0 / sum(count(USER_ID)) over (),2) as USER_DOUBLON_TOTAL_POURCENTAGE
+    count(DISTINCT USER_ID) as NOMBRE_ETUDIANT_TOTAL, -- total étudiants sans les doublons
+    count(USER_ID) - count(DISTINCT USER_ID) as NOMBRE_USER_TOTAL_DOUBLON, --total étudiants en doublons
+    round((count(USER_ID) - count(DISTINCT USER_ID)) * 100.0 / sum(count(USER_ID)) over (),2) as USER_DOUBLON_TOTAL_POURCENTAGE --pourcentage étudiants en doublons
 from {{ ref('stg_openclassrooms') }}
