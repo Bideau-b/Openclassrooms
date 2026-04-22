ef4a9e45454ffe861c5dd6c16494f01e77fe1d4a
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:37:14 +0000

    int

--- models/intermediate/int_insee_genre.sql
+++ models/intermediate/int_insee_genre.sql
@@ -1,7 +1,8 @@
+-- Calcul du pourcentage d'emplois par genre et par an 
 select 
         round(sum(NOMBRE_PERSONNES) * 100.0 / sum(sum(NOMBRE_PERSONNES)) over (Partition by ANNEE),2) as POURCENTAGE_GENRE_INSEE,
         GENRE,
         ANNEE
 from {{ ref('int_insee_genre_age_trt') }}
-WHERE GENRE<>'Ensemble'
+WHERE GENRE<>'Ensemble' --on ne prend que M et F
 GROUP BY GENRE,ANNEE
