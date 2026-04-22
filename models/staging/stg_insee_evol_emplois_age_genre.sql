c9e5d6b4ec4d2e2f62af81f608adc376062ac77b
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:39:04 +0000

    stg

--- models/staging/stg_insee_evol_emplois_age_genre.sql
+++ models/staging/stg_insee_evol_emplois_age_genre.sql
@@ -1,3 +1,5 @@
+--modif types de données en number et retrait des autres valeurs non numériques pour emplois total et les années
+
 select 
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM("ANNEE"),'[^0-9]+','')) AS ANNEE,
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM("F_15_24"),'[^0-9]+','')) AS F_15_24,
@@ -10,4 +12,4 @@ select
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM("H_50_64"),'[^0-9]+','')) AS H_50_64,
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM("E_50_64"),'[^0-9]+','')) AS E_50_64,
  from {{ source('snowflake','INSEE_EMPLOIS_AGE_GENRE')}}
- WHERE LENGTH(REGEXP_REPLACE(TRIM("ANNEE"), '[^0-9]+', '')) = 4
+ WHERE LENGTH(REGEXP_REPLACE(TRIM("ANNEE"), '[^0-9]+', '')) = 4 --selection des données avec 4 caractères (les années)
