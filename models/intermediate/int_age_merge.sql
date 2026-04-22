25e014b236b962f4cab0455d986db17bb1128650
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:36:57 +0000

    int

--- models/intermediate/int_age_merge.sql
+++ models/intermediate/int_age_merge.sql
@@ -1,3 +1,5 @@
+--chargement table openclassroom pour les données "age"
+
 with age_open as (
 
     select
@@ -7,6 +9,8 @@ with age_open as (
     from {{ ref('int_open_age') }}
 
 ),
+
+--chargement table INSEE pour les données "age"
 
 age_insee as (
     
@@ -18,6 +22,7 @@ age_insee as (
 
 )
 
+-- jointure des données openclassrooms avec ceux de l'INSEE sur les tranches d'ages
 
 select  
     POURCENTAGE_AGE_OPENCLASSROOM,
@@ -26,5 +31,5 @@ select
     ANNEE_DEBUT_FORMATION,
     ANNEE
 from age_open ao
-left join age_insee ai on ao.FOURCHETTE_AGE_REGROUPEE = ai.TRANCHE_AGE
-where ANNEE_DEBUT_FORMATION=ANNEE
+left join age_insee ai on ao.FOURCHETTE_AGE_REGROUPEE = ai.TRANCHE_AGE --jointure tranches d'ages
+where ANNEE_DEBUT_FORMATION=ANNEE --selection et liaison des années de 2022 à 2025
