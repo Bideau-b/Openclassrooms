de53b93dc4a895b1f53b2f6c5c28ae4885e93a4a
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:37:41 +0000

    int

--- models/intermediate/int_open_genre.sql
+++ models/intermediate/int_open_genre.sql
@@ -1,19 +1,9 @@
-with genre_temp as (
-
-    select
-        count(distinct USER_ID) as NOMBRE_USER, --on retire les doublons
-        round(count(distinct USER_ID)* 100 / sum(count(distinct USER_ID)) over (Partition by ANNEE_DEBUT_FORMATION),2) as USER_POURCENTAGE, 
-        GENRE,
-        ANNEE_DEBUT_FORMATION
-    from {{ ref('stg_openclassrooms') }}
-    WHERE GENRE<>'unknown' --On retire les données non remplis par les étudiants
-    GROUP BY GENRE,ANNEE_DEBUT_FORMATION
-
-)
+-- calcul du pourcentage d'étudiants par genre et par an (sans les doublons de formations)
 
 select
-    sum(USER_POURCENTAGE) as POURCENTAGE_GENRE_OPENCLASSROOM,
-    GENRE,
-    ANNEE_DEBUT_FORMATION
-FROM genre_temp
-GROUP BY GENRE, ANNEE_DEBUT_FORMATION
+        round(count(distinct USER_ID)* 100 / sum(count(distinct USER_ID)) over (Partition by ANNEE_DEBUT_FORMATION),2) as POURCENTAGE_GENRE_OPENCLASSROOM,
+        GENRE,
+        ANNEE_DEBUT_FORMATION
+from {{ ref('stg_openclassrooms') }}
+WHERE GENRE<>'unknown' --On retire les données non remplis par les étudiants
+GROUP BY GENRE,ANNEE_DEBUT_FORMATION
