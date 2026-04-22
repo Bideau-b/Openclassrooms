29ccd22c17bbb2ebfd014e5f945ba7425790ce95
Benjamin Bideau <benjamin.bideau@enedis.fr>
2026-04-22 07:38:55 +0000

    stg

--- models/staging/stg_insee_emplois_regions_2024.sql
+++ models/staging/stg_insee_emplois_regions_2024.sql
@@ -1,4 +1,7 @@
+--modif types de données en number et retrait des autres valeurs non numériques pour emplois total
 select 
     REGION,
     TRY_TO_NUMBER(REGEXP_REPLACE(TRIM("EMPLOIS_TOTAL"),'[^0-9]+','')) AS EMPLOIS_TOTAL,
 from {{ source('snowflake','INSEE_EMPLOIS_REGIONS_2024')}}
+WHERE REGION IN ('Martinique','La Réunion','Guyane','Guadeloupe','Mayotte','Bourgogne-Franche-Comté','Normandie','Centre-Val de Loire','Pays de la Loire','Bretagne','Provence-Alpes-Côte d''Azur','Nouvelle Aquitaine','Corse','Occitanie','Grand Est','Hauts-de-France','Auvergne-Rhône-Alpes','Île-de-France')
+and EMPLOIS_TOTAL is not null
